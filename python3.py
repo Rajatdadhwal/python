c82fb3d46b6ecd96ca367a87f91f1a1e12aac7c4
Rajat Dadhwal <rajatdadhwal201@gmail.com>
2022-10-11 00:59:53 +0530

    adding my file

---
+++ python3.py
@@ -0,0 +1,21 @@
+greeting=input("heyy")
+print("god bless you"    +   greeting)
+
+name = input("what is your name?")
+print("hello"   +     name)
+
+age=input("what is your age")
+print(age   +",nice")
+
+address=input("where are you from")
+print(address)
+
+qualification=input("what is your qualification")
+print(qualification)
+
+lastmeet=input("bye")
+print("byeee"    +    lastmeet)
+
+
+
+
