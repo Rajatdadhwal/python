a50ab4b2d390ec663aa959f07d573b04cad3a65d
Rajat Dadhwal <rajatdadhwal201@gmail.com>
2022-10-11 00:57:18 +0530

    adding my file

---
+++ python.1.py
@@ -0,0 +1,16 @@
+firstname = "rajat"
+secondname= "dadhwal"
+age=18
+is_genious= True 
+
+
+print(firstname)
+print(secondname)
+print(age)
+print(is_genious)
+
+name=input("what is your name?")
+print("hello"   +  name)
+
+hey=input("what is your superhero name?")
+print("hello"      +   hey)
