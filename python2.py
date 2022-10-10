cafd3b2a65afb91cbf3ce5b09cbadf751dd0b9f0
Rajat Dadhwal <rajatdadhwal201@gmail.com>
2022-10-11 00:58:38 +0530

    adding my file

---
+++ python2.py
@@ -0,0 +1,19 @@
+print("hello world")
+firstname= "sidhu moosewala"
+secondname = "rajat dadhwal"
+age= 18
+hobbies= "1st is youtube  2and is social media   3ard is coding"
+intro= "i am rajat dadhwal and i am from repur,datarpur<punjab>,,,    and last thing i am indian"
+myattitude= "i am ambivert"
+myphonenumber= 6284746637
+
+print(firstname)
+print(secondname)
+print(age)
+print(hobbies)
+print(intro)
+print(myattitude)
+print(myphonenumber)
+
+
+print(age)
