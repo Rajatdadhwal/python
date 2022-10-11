b7eda08c3adb5d72053c710d94d5e228d999910f
Rajat Dadhwal <rajatdadhwal201@gmail.com>
2022-10-11 21:15:06 +0530

    push to git hub

---
+++ practice4.py
@@ -0,0 +1,12 @@
+# 1. create a greeting for your program. 
+from tkinter import N
+
+
+print("welcome to band name generator")
+#2. ask the user for the city that they grow up im.
+city= input("from which city you grew up in?\n")
+#3. ask the user for their pet name. 
+pet= input("what is your pet name?\n")
+#4.  combine the name of their city and pet show them their band name.
+input("your band name could be " + city + pet)
+#5. make sure that the input cursor show on a new line. 
