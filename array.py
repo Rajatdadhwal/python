55a670e782a53c3262966f728f0d2eaf39b40e98
Rajat Dadhwal <rajatdadhwal201@gmail.com>
2022-10-16 15:38:01 +0530

    add

--- array.py
+++ array.py
@@ -1,12 +1,18 @@
 a = "i am rajat dadhwal and i am in lovely proffestional university"
 
 
-print(a[14])
+print(a[1])
 print(len(a))
+print(a[0:19])
 
 
-if "rajat" in a:
+
+if "rajat" in a :
     print("yess ! its there")
 
 else:
-    print("no ! its not there")    
+    print("no ! its not there")
+
+b= "hello world"
+
+print(b[-5:-2])
