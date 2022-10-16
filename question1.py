13099c9edc33e6fa44fe204333e9c961dc5ae125
Rajat Dadhwal <rajatdadhwal201@gmail.com>
2022-10-16 12:50:08 +0530

    adding my file

---
+++ question1.py
@@ -0,0 +1,11 @@
+s1=int(input("side1:"))
+s2=int(input("side1:"))
+s3=int(input("side1:"))
+
+if s1+s2>s3 or s2+s3>s1 or s3+s1>s2:
+    print("it is a triangle")
+else:
+    print("it is not a triangle")    
+
+
+
