6bdccd6ed3c89f2326b8f07e72008a2da1e796f9
Rajat Dadhwal <rajatdadhwal201@gmail.com>
2022-10-16 13:50:45 +0530

    adding my file

---
+++ array.py
@@ -0,0 +1,12 @@
+a = "i am rajat dadhwal and i am in lovely proffestional university"
+
+
+print(a[14])
+print(len(a))
+
+
+if "rajat" in a:
+    print("yess ! its there")
+
+else:
+    print("no ! its not there")    
