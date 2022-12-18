d29adf7a8517f3cc6a74acbedf6c6bd1f1feeb16
Rajat Dadhwal <rajatdadhwal201@gmail.com>
2022-12-18 13:07:34 +0530

    adding

--- question9.py
+++ question9.py
@@ -1,13 +1,15 @@
 #list1 = [10,20,30,40,50]
-#triple all the elements in the list
+#triple all the elements in the list:::::
 
 
 list1 = [10,20,30,40,50]
 output = list(map(lambda i : i * 3 , list1))
 print(output)
 
-# print out all the multiple of 5 using filter and lamda
+
+#list2 = [34,88,30,22,10,15,18] 
+# print out all the multiple of 5 using filter and lamda:::::
 
 list2 = [34,88,30,22,10,15,18]
 output = list(filter(lambda i : i % 5 == 0 , list2))
-print(output)
+print(output) 
